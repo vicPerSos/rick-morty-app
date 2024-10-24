1918f3ec4406e7c1bbe9b661ce1ae2da282f056f
vicPerSos <vpsb888@gmail.com>
2024-10-24 08:39:39 +0200

    Primera subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './components/character/character.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
